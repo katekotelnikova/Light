c5d01c2d0720b148e5b676c7f053d4e85a3407aa
Катрин <www.maleckiy33@gmail.com>
2018-09-28 23:17:03 +0300

    Реализовано изменение цвета фона при нажатии на клавишу

--- app/src/main/java/com/example/kev/light/MainActivity.java
+++ app/src/main/java/com/example/kev/light/MainActivity.java
@@ -1,13 +1,96 @@
 package com.example.kev.light;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.kev.light.R.color.colorYellow;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = "StartActivity";
+    Button red;
+    Button yellow;
+    Button green;
+    LinearLayout ll;
+    int bg;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ll = (LinearLayout) findViewById(R.id.l1);
+        red = (Button) findViewById(R.id.red);
+        yellow = (Button) findViewById(R.id.yellow);
+        green = (Button) findViewById(R.id.green);
+
+        red.setOnClickListener(this);
+        yellow.setOnClickListener(this);
+        green.setOnClickListener(this);
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        resetUI();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.red:
+                ll.setBackgroundColor(Color.RED);
+                bg = R.id.red;
+                break;
+            case R.id.yellow:
+                ll.setBackgroundColor(Color.YELLOW);
+                bg = R.id.yellow;
+                break;
+            case R.id.green:
+                ll.setBackgroundColor(Color.GREEN);
+                bg = R.id.green;
+                break;
+
+        }
+    }
+
+
 }
